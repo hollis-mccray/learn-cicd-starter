c1f30db7869c291ace245fb2dbcf998ea0671790
hollis-mccray <hollis.mccray@gmail.com>
2025-05-13 12:51:26 -0700

    Adding a unit test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		pass  bool
+		want  string
+	}{
+		{name: "Good header", input: "ApiKey 8675309", pass: true, want: "8675309"},
+		{name: "No key ", input: "", pass: false, want: ""},
+		{name: "Invalid authorization", input: "Speak, friend", pass: false, want: ""},
+	}
+
+	for _, tc := range tests {
+		head := http.Header{}
+		if tc.input != "" {
+			head.Add("Authorization", tc.input)
+		}
+
+		got, err := GetAPIKey(head)
+
+		if !tc.pass && err == nil {
+			t.Fatalf("%s: expected: fail, got: pass", tc.name)
+		} else if tc.pass && err == nil {
+			t.Fatalf("%s: expected: pass, got: fail %v", tc.name, err)
+		} else if got != tc.want {
+			t.Fatalf("%s: expectedP %s, got: %s", tc.name, tc.want, got)
+		}
+	}
+}
